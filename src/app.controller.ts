3ec682d70622e19aa9581c055b3bb17ecb7a0f55
Rohit Kumar <rkumar_be20@thapar.edu>
2023-03-13 15:54:17 +0530

    changed get request body to query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GetBodyDto, PostBodyDto, UpdateBodyDto } from './dto/request.dto';
 import { ResponseDataDto, ResponseUrlDto } from './dto/response.dto';
@@ -13,7 +13,7 @@ export class AppController {
   }
 
   @Get('get')
-  async getData(@Body() body: GetBodyDto): Promise<ResponseDataDto> {
+  async getData(@Query() body: GetBodyDto): Promise<ResponseDataDto> {
     console.log('Call Get Request ' + body);
     const data = await this.appService.getData(body);
     console.log('Sending ' + data);
